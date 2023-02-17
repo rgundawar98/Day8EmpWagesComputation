adad9b85f931a7be4038da2acc4829967143abcf
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-02-17 16:19:08 +0530

    Calculating daily wage using switch UC4_SwitchCse

--- Day8EmpWagesCompuatation/EmpWages.cs
+++ Day8EmpWagesCompuatation/EmpWages.cs
@@ -8,29 +8,29 @@ namespace Day8EmpWagesCompuatation
 {
     public class EmpWages
     {
+         const int Is_FullTime = 1;
+         const int Is_PartTime = 2;
+         const int Emp_Per_Hour = 20;
         public void Employee()
         {
-            int Is_FullTime = 1;
-            int Is_PartTime = 2;
-            int Emp_Per_Hour = 20;
             int Emp_Wage = 0;
             int Emp_Hr = 0;
             Random random   = new Random();
             int Emp_Check = random.Next(3);
-            if(Emp_Check == Is_FullTime)
+            switch(Emp_Check)
             {
-                Console.WriteLine("Employee is Full Time");
-                Emp_Hr = 8;
-            }
-            else if(Emp_Check == Is_PartTime) 
-            {
-                Console.WriteLine("Employee is Part Time");
-                Emp_Hr = 4;
-            }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
-                Emp_Hr = 0;
+                case Is_FullTime:
+                   Console.WriteLine("Employee is Full Time");
+                    Emp_Hr = 8;
+                    break;
+                case Is_PartTime:
+                    Console.WriteLine("Employee is Part Time");
+                    Emp_Hr = 4;
+                    break;
+                default:
+                     Console.WriteLine("Employee is Absent");
+                     Emp_Hr = 0;
+                     break;
             }
             Emp_Wage = Emp_Hr * Emp_Per_Hour;
             Console.WriteLine("Daily wage will be:"+Emp_Wage);
