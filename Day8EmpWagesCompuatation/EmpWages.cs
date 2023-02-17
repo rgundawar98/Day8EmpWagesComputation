27a30d2b02b3ff65fa6850ca51eb9a6c2d19de4d
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-02-17 16:09:57 +0530

    Calculate daily wage UC2_DailyWage

--- Day8EmpWagesCompuatation/EmpWages.cs
+++ Day8EmpWagesCompuatation/EmpWages.cs
@@ -11,16 +11,23 @@ namespace Day8EmpWagesCompuatation
         public void Employee()
         {
             int Is_Present = 1;
+            int Emp_Per_Hour = 20;
+            int Emp_Wage = 0;
+            int Emp_Hr = 0;
             Random random   = new Random();
             int Emp_Check = random.Next(2);
             if(Emp_Check == Is_Present)
             {
                 Console.WriteLine("Employee is present");
+                Emp_Hr = 8;
             }
             else
             {
                 Console.WriteLine("Employee is absent");
+                Emp_Hr = 0;
             }
+            Emp_Wage = Emp_Hr * Emp_Per_Hour;
+            Console.WriteLine("Daily wage will be:"+Emp_Wage);
         }
     }
 }
