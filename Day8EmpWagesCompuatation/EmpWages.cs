19f07c752913fb66116af4b262347398b51b33bf
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-02-17 16:13:46 +0530

    Adding part time employee and calculates wage UC3_PartTime

--- Day8EmpWagesCompuatation/EmpWages.cs
+++ Day8EmpWagesCompuatation/EmpWages.cs
@@ -10,20 +10,26 @@ namespace Day8EmpWagesCompuatation
     {
         public void Employee()
         {
-            int Is_Present = 1;
+            int Is_FullTime = 1;
+            int Is_PartTime = 2;
             int Emp_Per_Hour = 20;
             int Emp_Wage = 0;
             int Emp_Hr = 0;
             Random random   = new Random();
-            int Emp_Check = random.Next(2);
-            if(Emp_Check == Is_Present)
+            int Emp_Check = random.Next(3);
+            if(Emp_Check == Is_FullTime)
             {
-                Console.WriteLine("Employee is present");
+                Console.WriteLine("Employee is Full Time");
                 Emp_Hr = 8;
+            }
+            else if(Emp_Check == Is_PartTime) 
+            {
+                Console.WriteLine("Employee is Part Time");
+                Emp_Hr = 4;
             }
             else
             {
-                Console.WriteLine("Employee is absent");
+                Console.WriteLine("Employee is Absent");
                 Emp_Hr = 0;
             }
             Emp_Wage = Emp_Hr * Emp_Per_Hour;
