58a29fd39d918e74d8f1d9b0715926e6306c8983
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-02-17 16:06:00 +0530

    Checking employee is present or absent UC1_CheckEmpAttendance

---
+++ Day8EmpWagesCompuatation/EmpWages.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8EmpWagesCompuatation
+{
+    public class EmpWages
+    {
+        public void Employee()
+        {
+            int Is_Present = 1;
+            Random random   = new Random();
+            int Emp_Check = random.Next(2);
+            if(Emp_Check == Is_Present)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
--- Day8EmpWagesCompuatation/Program.cs
+++ Day8EmpWagesCompuatation/Program.cs
@@ -7,6 +7,8 @@ namespace Day8EmpWagesCompuatation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Employee Wage Compuatation");
+            EmpWages emp = new EmpWages(); //Creatimg the object
+            emp.Employee(); //Calling the method using reference object
         }
     }
 }
