94aa74c47835ca70d36b281a9173c957987c07d2
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-02-17 15:58:34 +0530

    Adding welcome messagr

---
+++ Day8EmpWagesCompuatation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day8EmpWagesCompuatation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Employee Wage Compuatation");
+        }
+    }
+}
